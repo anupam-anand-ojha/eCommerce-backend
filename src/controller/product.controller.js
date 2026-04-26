e92a7fd7debde23ae5ff881a73b94ebb9c31033f
Anupam Anand <theanupamojha999@gmail.com>
2026-04-26 23:29:41 +0530

    add product controller

---
+++ src/controller/product.controller.js
@@ -0,0 +1,17 @@
+
+import Product from "../models/product.model.js";
+
+export const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.json(product);
+};
+
+export const getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+export const getSingleProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.json(product);
+};
