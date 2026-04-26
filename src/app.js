ca397a6823159672e60387802fe3b4110dcd8516
Anupam Anand <theanupamojha999@gmail.com>
2026-04-27 01:22:27 +0530

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import orderRoutes from "./routes/order.route.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
 
 export default app;
