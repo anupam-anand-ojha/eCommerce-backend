0ff879413ed965f99ea9b903af5a87be97f0a0a4
Anupam Anand <theanupamojha999@gmail.com>
2026-04-26 23:08:35 +0530

    set modals

---
+++ src/model/user.model.js
@@ -0,0 +1,19 @@
+// models/user.model.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+
+  },
+  Email: {
+     type: String,
+     unique: true,
+  },
+  password:{
+    type: String
+  },
+  isAdmin: { type: Boolean, default: false }
+});
+
+export default mongoose.model("User", userSchema);
